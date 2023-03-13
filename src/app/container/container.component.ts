76c9e8b0a4fba996aa89a4415d45498376281f24
Filip <Filip>
2023-03-13 09:00:44 +0100

    ai doesn't work

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -6,6 +6,13 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./container.component.css'],
 })
 export class ContainerComponent {
+  // humanPlayer: number = ;
+  // computerPlayer: number;
+  // constructor(humanPlayer: number, computerPlayer: number) {
+  //   this.humanPlayer = humanPlayer;
+  //   this.computerPlayer = computerPlayer;
+  //   this.reset();
+  // }
   @Output() output = new EventEmitter<number[]>();
   player: string = '';
   x: boolean = true;
@@ -16,18 +23,24 @@ export class ContainerComponent {
   clicked(n: number, i: number, j: number) {
     this.board[i][j] = n;
 
-    if (this.x) {
-      this.player = 'O';
-      this.x = false;
-    } else {
-      this.player = 'X';
-      this.x = true;
+    this.makeMove(2);
+
+    // if (this.x) {
+    //   this.player = 'O';
+    //   this.x = false;
+    // } else {
+    //   this.player = 'X';
+    //   this.x = true;
+    // }
+
+    if (this.checkWinner(this.board, 1)) {
+      this.xScore++;
+    }
+    if (this.checkWinner(this.board, 2)) {
+      this.oScore++;
     }
 
-    if (this.checkWinner(this.board, n)) {
-      n === 1 ? this.xScore++ : this.oScore++;
-    }
-    console.log(this.xScore, this.oScore);
+    // console.log(this.xScore, this.oScore);
     this.output.emit([this.xScore, this.oScore]);
   }
   checkWinner(board: number[][], n: number): boolean {
@@ -109,6 +122,98 @@ export class ContainerComponent {
     // Nie znaleziono zwycięzcy
     return false;
   }
+
+  // ================================================================================
+
+  winningCombinations: number[][] = [
+    [0, 1, 2, 3, 4],
+    [10, 11, 12, 13, 14],
+    [20, 21, 22, 23, 24],
+    [30, 31, 32, 33, 34],
+    [40, 41, 42, 43, 44],
+    [0, 10, 20, 30, 40],
+    [1, 11, 21, 31, 41],
+    [2, 12, 22, 32, 42],
+    [3, 13, 23, 33, 43],
+    [4, 14, 24, 34, 44],
+    [0, 11, 22, 33, 44],
+    [4, 13, 22, 31, 40],
+  ];
+
+  reset(): void {
+    for (let i = 0; i < 10; i++) {
+      this.board[i] = [];
+    }
+  }
+
+  getAvailableMoves(): number[][] {
+    console.log('here');
+
+    const moves: number[][] = [];
+    for (let i = 0; i < 10; i++) {
+      // console.log(i);
+
+      for (let j = 0; j < 10; j++) {
+        if (this.board[i][j] === 0) {
+          moves.push([i, j]);
+        }
+      }
+    }
+    // console.log('moves ' + moves);
+
+    return moves;
+  }
+
+  isGameOver(): boolean {
+    const availableMoves = this.getAvailableMoves();
+    return availableMoves.length === 0;
+  }
+  getWinningPlayer(): number {
+    return this.checkWinner(this.board, 2) ? 2 : 0;
+  }
+
+  evaluateMove(player: number): Move {
+    if (this.isGameOver()) {
+      if (this.getWinningPlayer() === 2) {
+        return { index: [0], score: 10 };
+      } else if (this.getWinningPlayer() === 1) {
+        return { index: [0], score: -10 };
+      } else {
+        return { index: [0], score: 0 };
+      }
+    }
+
+    const moves: Move[] = [];
+
+    for (const move of this.getAvailableMoves()) {
+      this.board[move[0]][move[1]] = 2;
+      const score = this.evaluateMove(1).score;
+      moves.push({ index: move, score: score });
+      this.board[move[0]][move[1]] = 0;
+    }
+
+    let bestMove: Move = { index: [0], score: 0 };
+    let bestScore = -Infinity;
+
+    for (const move of moves) {
+      if (move.score > bestScore) {
+        bestScore = move.score;
+        bestMove = move;
+      }
+    }
+
+    return bestMove;
+  }
+
+  makeMove(player: number): void {
+    // console.log(player);
+
+    const move = this.evaluateMove(player);
+    // console.log(move);
+
+    // this.board[move.index[0]][move.index[1]] = player;
+  }
+
   ngOnInit(): void {
     this.player = 'X';
     for (let i = 0; i < 10; i++) {
@@ -119,3 +224,16 @@ export class ContainerComponent {
     }
   }
 }
+// interface Board {
+//   [key: string]: string;
+// }
+interface Move {
+  index: number[];
+  score: number;
+}
+
+// Example usage
+
+// Computer makes move
+// Human makes move
+// ...continue the game
